6e8de5dbbf74e583fec317e2d3ee6c41224eccbe
Berardinux <berardinux@gmail.com>
2024-08-15 17:31:04 -0400

    Aug 15, 17:15

--- code/Python/Controller/src/StepperController.py
+++ code/Python/Controller/src/StepperController.py
@@ -29,16 +29,23 @@ gamepad = InputDevice('/dev/input/event4')  # Change to your specific event file
 
 moving = False  # Track whether the motor should be moving
 
-def handle_limit_switch():
+def limitSwitch():
     if GPIO.input(DIR0) == GPIO.HIGH:
         print("Limit switch DIR0 triggered")
-        GPIO.output(DIR, GPIO.HIGH)  # Reverse direction
-        pwm.ChangeDutyCycle(50)  # Keep the motor moving
-
+        while GPIO.input(DIR0) == GPIO.HIGH:
+            print("Edge or Rail")
+            GPIO.output(DIR, GPIO.HIGH)
+            pwm.ChangeDutyCycle(50)
+#            while absevent.event.value == RIGHT and GPIO.input(DIR0) == GPIO.LOW:
+#                pwm.ChangeDutyCycle(0)
     elif GPIO.input(DIR1) == GPIO.HIGH:
         print("Limit switch DIR1 triggered")
-        GPIO.output(DIR, GPIO.LOW)  # Reverse direction
-        pwm.ChangeDutyCycle(50)  # Keep the motor moving
+        while GPIO.input(DIR1) == GPIO.HIGH:
+            print("Edge or Rail")
+            GPIO.output(DIR, GPIO.LOW)
+            pwm.ChangeDutyCycle(50)
+#           while absevent.event.value == LEFT and GPIO.input(DIR1) == GPIO.LOW:
+#                pwm.ChangeDutyCycle(0)
 
 try:
     for event in gamepad.read_loop():
@@ -49,7 +56,7 @@ try:
                 # Check if limit switches are triggered
                 if absevent.event.value == LEFT:  # D-pad left
                     if GPIO.input(DIR1) == GPIO.HIGH:
-                        handle_limit_switch()
+                        limitSwitch()
                     else:
                         print("D-pad left pressed")
                         GPIO.output(DIR, GPIO.LOW)  # Set direction to LOW
@@ -57,7 +64,7 @@ try:
                         moving = True
                 elif absevent.event.value == RIGHT:  # D-pad right
                     if GPIO.input(DIR0) == GPIO.HIGH:
-                        handle_limit_switch()
+                        limitSwitch()
                     else:
                         print("D-pad right pressed")
                         GPIO.output(DIR, GPIO.HIGH)  # Set direction to HIGH
