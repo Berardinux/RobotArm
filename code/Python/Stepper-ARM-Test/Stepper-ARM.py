75f793db810baa203da3d6930f7f936af3630ba9
Berardinux <berardinux@gmail.com>
2024-08-18 14:39:54 -0400

    Aug 18, 14:38

--- code/Python/Stepper-ARM-Test/Stepper-ARM.py
+++ code/Python/Stepper-ARM-Test/Stepper-ARM.py
@@ -3,16 +3,16 @@ import RPi.GPIO as GPIO
 
 DIR = 14
 STEP = 15
-DIR0 = 18
-DIR1 = 23
+LIMIT_SWITCH_0 = 18
+LIMIT_SWITCH_1 = 23
 i = 0  # Start with i = 0
 
 # Setup GPIO
 GPIO.setmode(GPIO.BCM)  # Use GPIO numbers
 GPIO.setup(DIR, GPIO.OUT)  # Direction pin
 GPIO.setup(STEP, GPIO.OUT)  # Step pin (PWM)
-GPIO.setup(DIR0, GPIO.IN)   # Input pin to change direction
-GPIO.setup(DIR1, GPIO.IN)   # Input pin to change direction
+GPIO.setup(LIMIT_SWITCH_0, GPIO.IN)   # Input pin to change direction
+GPIO.setup(LIMIT_SWITCH_1, GPIO.IN)   # Input pin to change direction
 
 # Initialize PWM on STEP pin with a frequency of 1000 Hz
 pwm = GPIO.PWM(STEP, 1000)  # 1000 Hz frequency
@@ -20,6 +20,9 @@ pwm.start(50)  # Start PWM with a 50% duty cycle
 
 # Set initial direction
 GPIO.output(DIR, GPIO.HIGH)
+
+def Ramp():
+
 
 try:
     while True:
@@ -33,13 +36,13 @@ try:
         # Run the motor with PWM
         pwm.ChangeDutyCycle(50)  # You can adjust the duty cycle as needed
 
-        # Check if either pin DIR0 or DIR1 goes high
-        if GPIO.input(DIR0) == GPIO.HIGH:
-            print("GPIO DIR0 went HIGH")
+        # Check if either pin LIMIT_SWITCH_0 or LIMIT_SWITCH_1 goes high
+        if GPIO.input(LIMIT_SWITCH_0) == GPIO.HIGH:
+            print("GPIO LIMIT_SWITCH_0 went HIGH")
             GPIO.output(DIR, GPIO.HIGH)  # Switch direction
             sleep(0.5)  # Small delay to debounce the input
-        elif GPIO.input(DIR1) == GPIO.HIGH:
-            print("GPIO DIR1 went HIGH")
+        elif GPIO.input(LIMIT_SWITCH_1) == GPIO.HIGH:
+            print("GPIO LIMIT_SWITCH_1 went HIGH")
             GPIO.output(DIR, GPIO.LOW)  # Switch direction
             sleep(0.5)  # Small delay to debounce the input
 
