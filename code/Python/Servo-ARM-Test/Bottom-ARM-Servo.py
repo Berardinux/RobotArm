da1d1948030b85e40bb9ea3571e914875bd7297d
Berardinux <berardinux@gmail.com>
2024-08-20 19:00:36 -0400

    Aug 20, 18:17

--- code/Python/Servo-ARM-Test/Bottom-ARM-Servo.py
+++ code/Python/Servo-ARM-Test/Bottom-ARM-Servo.py
@@ -22,12 +22,12 @@ def Ramp(OldDutyCycle, NewDutyCycle):
             i -= 0.01  # Decrement by 0.1
             sleep(.01)
 # - = back + forward
-# 4.7 is 180 degrees 
+# 4.8 is 180 degrees 
 
 try:
   servo.ChangeDutyCycle(4.8)
   sleep(1)
-  Ramp(4.6, 4.8)
+  Ramp(4.8, 7)
   sleep(1)
   #servo.ChangeDutyCycle(4)
   #time.sleep(1)
