cc809cd5ad7688ee239591c5982ac93dde4bbfbe
Berardinux <berardinux@gmail.com>
2024-08-20 18:42:27 -0400

    Aug 20, 18:17

--- code/Python/Servo-ARM-Test/Middle-ARM-Servo.py
+++ code/Python/Servo-ARM-Test/Middle-ARM-Servo.py
@@ -25,9 +25,9 @@ def Ramp(OldDutyCycle, NewDutyCycle):
 # 6.2 in 180 degrees
 
 try:
-  servo.ChangeDutyCycle(9)
+  servo.ChangeDutyCycle(9.3)
   sleep(1)
-  Ramp(9, 9.3)
+  Ramp(9.3, 9.2)
   sleep(1)
   #servo.ChangeDutyCycle(4)
   #time.sleep(1)
