c96ef1d05b95ef7d4e4721c893a05931600bb7aa
Berardinux <berardinux@gmail.com>
2024-08-20 17:57:00 -0400

    Aug 20, 17:24

--- code/Python/Servo-ARM-Test/Bottom-ARM-Servo.py
+++ code/Python/Servo-ARM-Test/Bottom-ARM-Servo.py
@@ -6,8 +6,24 @@ GPIO.setup( 21 ,GPIO.OUT)
 servo = GPIO.PWM( 21, 50)
 servo.start(0)
 
+def Ramp(OldDutyCycle, NewDutyCycle):
+    if OldDutyCycle < NewDutyCycle:
+        # Ramp up
+        i = OldDutyCycle
+        while i <= NewDutyCycle:
+            servo.ChangeDutyCycle(i)
+            i += 0.1  # Increment by 0.1
+    elif OldDutyCycle > NewDutyCycle:
+        # Ramp down
+        i = OldDutyCycle
+        while i >= NewDutyCycle:
+            servo.ChangeDutyCycle(i)
+            i -= 0.1  # Decrement by 0.1
+
+  
+
 try:
-  servo.ChangeDutyCycle(2)
+  servo.ChangeDutyCycle(4)
   time.sleep(1)
   #servo.ChangeDutyCycle(4)
   #time.sleep(1)
