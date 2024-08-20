faefdaf38227ef3a8191dce45d2c50ac3db1e4a4
Berardinux <berardinux@gmail.com>
2024-08-20 18:58:15 -0400

    Aug 20, 18:17

--- code/Python/Servo-ARM-Test/Middle-ARM-Servo.py
+++ code/Python/Servo-ARM-Test/Middle-ARM-Servo.py
@@ -22,15 +22,15 @@ def Ramp(OldDutyCycle, NewDutyCycle):
             i -= 0.01  # Decrement by 0.1
             sleep(.01)
 
-# (- = back) (+ = forward, end of travel is 10.8)
+# (- = back, end of travel is 2) (+ = forward, end of travel is 10.8)
 # 6.2 is 180 degrees
 # 9   is 90  degrees
 # 3.2 is 270 degrees
 
 try:
-  servo.ChangeDutyCycle(3)
+  servo.ChangeDutyCycle(2)
   sleep(1)
-  Ramp(3, 2)
+  Ramp(2, 6.2)
   sleep(1)
   #servo.ChangeDutyCycle(4)
   #time.sleep(1)
