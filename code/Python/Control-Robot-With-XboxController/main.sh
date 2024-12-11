cca3785b3004dd1d99e89cd88047d20b4d7023d2
Berardinux <berardinux@gmail.com>
2024-12-11 00:58:26 +0000

    241210-1958

--- code/Python/Control-Robot-With-XboxController/main.sh
+++ code/Python/Control-Robot-With-XboxController/main.sh
@@ -6,10 +6,10 @@ isControllerConnected=$(ls /dev/input | grep event4)
 # Check if the Xbox Controller is connected.
 if [ -n "$isControllerConnected" ]; then
     echo "Command produced isControllerConnected: $isControllerConnected"
-    /usr/bin/python3 /home/berardinux/RobotArm/code/Python/Controller/src/Arm/IndividualControl/main.py &
-    /usr/bin/python3 /home/berardinux/RobotArm/code/Python/Controller/src/ArmBase/main.py &
-    /usr/bin/python3 /home/berardinux/RobotArm/code/Python/Controller/src/Hand/main.py &
-    /usr/bin/python3 /home/berardinux/RobotArm/code/Python/Controller/src/Stepper/StepperController.py &
+    /usr/bin/python3 /home/berardinux/RobotArm/code/Python/Control-Robot-With-XboxController/src/Arm/IndividualControl/main.py &
+    /usr/bin/python3 /home/berardinux/RobotArm/code/Python/Control-Robot-With-XboxController/src/ArmBase/main.py &
+    /usr/bin/python3 /home/berardinux/RobotArm/code/Python/Control-Robot-With-XboxController/src/Hand/main.py &
+    /usr/bin/python3 /home/berardinux/RobotArm/code/Python/Control-Robot-With-XboxController/src/Stepper/StepperController.py &
     while [ -n "$isControllerConnected" ]; do
       isControllerConnected=$(ls /dev/input | grep event4)
       echo "The Xbox Controller is still connected"
